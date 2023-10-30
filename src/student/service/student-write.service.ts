1622962cfa89e952b5f75cece80955438480f65a
Sven Mayer <masv1019@h-ka.de>
2023-10-30 11:36:32 +0100

    cleanup and format code

--- src/student/service/student-write.service.ts
+++ src/student/service/student-write.service.ts
@@ -19,6 +19,7 @@ import { StudentReadService } from './student-read.service.js';
 // eslint-disable-next-line sort-imports
 import { InjectRepository } from '@nestjs/typeorm';
 import { MailService } from '../../mail/mail.service.js';
+// eslint-disable-next-line @typescript-eslint/naming-convention
 import RE2 from 're2';
 import { getLogger } from '../../logger/logger.js';
 
