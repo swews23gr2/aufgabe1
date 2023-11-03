26175f01c090608cd9df8573fae926f9e12a300d
Nicolasbu <nicolas.busch2000@web.de>
2023-11-03 16:47:34 +0100

    sqlite-implementiert

--- src/config/resources/db/sqlite/drop.sql
+++ src/config/resources/db/sqlite/drop.sql
@@ -14,10 +14,10 @@
 -- along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 -- https://www.sqlite.org/lang_dropindex.html
-DROP INDEX IF EXISTS abbildung_buch_id_idx;
-DROP INDEX IF EXISTS buch_isbn_idx;
+DROP INDEX IF EXISTS fach_student_id_idx;
+DROP INDEX IF EXISTS student_matrikel_idx;
 
 -- https://www.sqlite.org/lang_droptable.html
-DROP TABLE IF EXISTS abbildung;
-DROP TABLE IF EXISTS titel;
-DROP TABLE IF EXISTS buch;
+DROP TABLE IF EXISTS fach;
+DROP TABLE IF EXISTS adresse;
+DROP TABLE IF EXISTS student;
